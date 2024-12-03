30337dd1c0d130d6d4146f4e6c5b31805904ff9d
ReshmiRahul <reshmihl94@gmail.com>
2024-12-03 14:51:10 -0500

    Adding views for edit

--- CoreEntityFramework/obj/Debug/net8.0/PetAdoption.AssemblyInfo.cs
+++ CoreEntityFramework/obj/Debug/net8.0/PetAdoption.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PetAdoption")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da64acc4379d4c1f83c502eae5b14cdfb2beed21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a87f28b1fe1ff4a4076820e0f5765be60a25d13")]
 [assembly: System.Reflection.AssemblyProductAttribute("PetAdoption")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PetAdoption")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
